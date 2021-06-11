075ead3d1609c38ee4e458dac318b35f74fdf1e4
aaisara12 <68300841+aaisara12@users.noreply.github.com>
2021-06-11 14:59:53 -0700

    Basic string shape mechanics
    String/line between players that stretches and shrinks as players move

---
+++ GMTK_2021_Project/Assets/Scripts/StringController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StringController : MonoBehaviour
+{
+    LineRenderer lineRenderer;
+    EdgeCollider2D edgeCollider;
+    [SerializeField] Transform player;
+    [SerializeField] Transform altPlayer;
+
+    Vector2 firstPos = Vector2.zero;
+    Vector2 secondPos = Vector2.zero;
+
+    void Awake()
+    {
+        //TODO: Initialize line renderer and edge collider
+        lineRenderer = GetComponent<LineRenderer>();
+        edgeCollider = GetComponent<EdgeCollider2D>();
+
+        if(lineRenderer != null)
+            lineRenderer.positionCount = 2;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        firstPos = new Vector2(player.position.x, player.position.y);
+        secondPos = new Vector2(altPlayer.position.x, altPlayer.position.y);
+
+        if(lineRenderer != null)
+        {
+            lineRenderer.SetPosition(0, firstPos);
+            lineRenderer.SetPosition(1, secondPos);
+        }
+
+        if(edgeCollider != null)
+        {
+            Vector2[] newPoints = {firstPos, secondPos};
+            edgeCollider.points = newPoints;
+        }
+    }
+}
