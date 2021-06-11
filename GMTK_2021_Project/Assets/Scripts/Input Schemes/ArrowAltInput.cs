0accc42eecacfeec41c70dcd2f0f74f046a61e2d
aaisara12 <68300841+aaisara12@users.noreply.github.com>
2021-06-11 15:42:58 -0700

    Click input scheme
    Implemented an additional prototype input scheme based on click position (similar to League) to explore other input options

--- GMTK_2021_Project/Assets/Scripts/Input Schemes/ArrowAltInput.cs
+++ GMTK_2021_Project/Assets/Scripts/Input Schemes/ArrowAltInput.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AltPlayerInput : MonoBehaviour, IUnitInput
+public class ArrowAltInput : MonoBehaviour, IUnitInput
 {
     public Vector2 inputVector => new Vector2(Input.GetAxisRaw("Alt_Horizontal"), Input.GetAxisRaw("Alt_Vertical"));
 }
--- GMTK_2021_Project/Assets/Scripts/Input Schemes/ArrowInput.cs
+++ GMTK_2021_Project/Assets/Scripts/Input Schemes/ArrowInput.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInput : MonoBehaviour, IUnitInput
+public class ArrowInput : MonoBehaviour, IUnitInput
 {
     public Vector2 inputVector => new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
