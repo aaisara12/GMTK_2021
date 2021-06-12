d15e8cf9bac9251defc877c0fed84a37483f07a1
aaisara12 <68300841+aaisara12@users.noreply.github.com>
2021-06-11 18:37:45 -0700

    Projectile pooling system
    Implemented object pooling system for projectiles (from Jason Weimann's object pooling tutorial) in order to reduce CPU load from creating/destroying projectiles

--- GMTK_2021_Project/Assets/Scripts/Projectile.cs
+++ GMTK_2021_Project/Assets/Scripts/Projectile.cs
@@ -2,9 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+public class Projectile : MonoBehaviour, IPooledGameObject
 {
     [SerializeField] float force = 1;
+    [SerializeField] float expireTime = 5;
+    float timeOfEnable = 0;
+
+
+    // NOTE:  We use the below structure in order to catch mistakenly assigning a pool more than once to a given projectile
+    private ProjectilePool _pool;       // Note that we need this private version of the pool instance in order to make the check whether a pool has been assigned to it
+    public ProjectilePool pool
+    {
+        get{ return _pool;}
+        set
+        {
+            if(_pool == null)
+                _pool = value;
+            else
+                throw new System.Exception("Attempted to assign multiple pools to the same projectile!");   // A pool should only be assigned once after the instantiation of the projectile by the pooling system
+        }
+    }
 
 
     // Update is called once per frame
@@ -12,6 +29,28 @@ public class Projectile : MonoBehaviour
     void OnEnable()
     {
         GetComponent<Rigidbody2D>().AddForce(transform.right * force);
+        timeOfEnable = Time.time;
+
+        Debug.Log("ENABLING PROJ");
     }
 
+
+    void Update()
+    {
+        if(Time.time - timeOfEnable > expireTime)
+        {
+            pool.ReturnToPool(gameObject);
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+    }
+
+
+
+
+}
+
+// This interface allows us to give any game object the "credentials" it needs to be accepted/used as a pooled object
+public interface IPooledGameObject
+{
+    ProjectilePool pool {get; set;}
 }
